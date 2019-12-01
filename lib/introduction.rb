1c5fa6445db244b6dd1fc1802310319abb0fd1d6
Bruno Silva <brunosilvinosilva@gmail.com>
2019-12-01 15:52:07 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -9,6 +9,11 @@ end
 
 introduction("Josh")
 
+def introduction_with_language(name) + (language)
+ puts "Hi, my name is #{name} and I'm learning to program with #{language}"
+end
+
+introduction_with_language("Josh")("React")
 
 
 
